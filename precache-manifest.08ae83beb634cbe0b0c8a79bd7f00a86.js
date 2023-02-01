e3e93c7d8391abe8e1a4ab29333177a3b0c179e2
grigorydobrenko <grigorydobrenko97@gmail.com>
2023-02-01 13:02:03 +0300

    Updates

---
+++ precache-manifest.08ae83beb634cbe0b0c8a79bd7f00a86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9e03cea361b60a96218b2b6cecf543",
+    "url": "/todolist-redux/index.html"
+  },
+  {
+    "revision": "a5d85e96ae2345b70d9b",
+    "url": "/todolist-redux/static/css/main.9a50b660.chunk.css"
+  },
+  {
+    "revision": "3764c4b9f4bc154a68f3",
+    "url": "/todolist-redux/static/js/2.fded7f28.chunk.js"
+  },
+  {
+    "revision": "6127279098a66fe146baae54c06b7218",
+    "url": "/todolist-redux/static/js/2.fded7f28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5d85e96ae2345b70d9b",
+    "url": "/todolist-redux/static/js/main.26cb8fce.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/todolist-redux/static/js/main.26cb8fce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4434a15131a76001ef7f",
+    "url": "/todolist-redux/static/js/runtime-main.4e919186.js"
+  }
+]);
